8c9cc707f7e7f1ab04a0c68d83791250c0102c01
jbh1996 <jacob.b.hopkins.96@gmail.com>
2025-02-06 16:33:50 -0800

    Initial Microservice Work

--- example_call.py
+++ example_call.py
@@ -10,13 +10,8 @@ for num in range(10):
     start_time = time.time()
     start_time -= random.randint(15, 700)
     flash_cards = random.randint(6, 50)
-    print(f"Sending Request for {flash_cards}, and time: {start_time}")
+    print(f"Sending request for {flash_cards} flash cards, and time: {start_time}")
     time_string = f"{flash_cards} {start_time}"
     socket.send_string(time_string)
-    #  Get the reply.
-    message = socket.recv_string()
-    print(message)
-    socket.send_string(time_string)
-    #  Get the reply.
     message = socket.recv_string()
     print(message)
